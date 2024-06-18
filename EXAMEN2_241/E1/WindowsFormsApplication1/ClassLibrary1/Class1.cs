b0d4a7b77b4c71d06b6b019d38ea062e5ee6b367
AlexGit2004 <130529784+AlexGit2004@users.noreply.github.com>
2024-06-18 17:51:37 -0400

    Add files via upload

---
+++ EXAMEN2_241/E1/WindowsFormsApplication1/ClassLibrary1/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        string connectionString = "Data Source=(local);Initial Catalog=academico;Integrated Security=True;";
+
+        public Class1()
+        {
+        
+        }
+
+        
+
+
+        public DataTable ExecuteSelectQuery(string tableName)
+        {
+            DataTable dataTable = new DataTable();
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string query = "SELECT * FROM "+tableName;
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        using (SqlDataAdapter dataAdapter = new SqlDataAdapter(command))
+                        {
+                            dataAdapter.Fill(dataTable);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Manejo de excepciones
+                throw new Exception("Error al ejecutar la consulta SQL", ex);
+            }
+
+            return dataTable;
+        }
+    }
+}
---
+++ EXAMEN2_241/E1/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using ClassLibrary1;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Class1 clase = new Class1();
+
+            DataTable dataTable = clase.ExecuteSelectQuery("estudiante");
+            dataGridView1.DataSource = dataTable;
+            
+        }
+
+    }
+}
